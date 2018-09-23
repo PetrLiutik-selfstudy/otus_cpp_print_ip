2b5c83ab79a869c3cb8b6530715e5a8737d8c72d
Petr <petr_lutik@mail.ru>
2018-09-23 19:32:58 +0400

    Начал добавлять тесты

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,8 @@
 #include "gtest/gtest.h"
-#include "../inc/ver.h"
 #include "../inc/print_ip.h"
+#include "../inc/ver.h"
+
+#include <sstream>
 
 TEST(ver_test_case, ver_major_test) {
   EXPECT_GE(ver_major(), 1);
