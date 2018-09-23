730ac30e3e8bdc5004c18ec8adac7614d229a050
Petr <petr_lutik@mail.ru>
2018-09-23 22:07:25 +0400

    Добален тест для std::tuple

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -47,10 +47,18 @@ TEST(print_ip_test_case, print_ip_int_test) {
   uint8_t ip4{11};
   std::string ip4_str{"11"};
   EXPECT_EQ(get_ip_string(ip4), ip4_str);
-  
+
   int16_t ip5{-1111};
   std::string ip5_str{"251.169"};
   EXPECT_EQ(get_ip_string(ip5), ip5_str);
+
+  int32_t ip6{-111111111};
+  std::string ip6_str{"249.96.148.57"};
+  EXPECT_EQ(get_ip_string(ip6), ip6_str);
+
+  uint64_t ip7{1111111111111111111};
+  std::string ip7_str{"15.107.117.171.43.196.113.199"};
+  EXPECT_EQ(get_ip_string(ip7), ip7_str);
 }
 
 TEST(print_ip_test_case, print_ip_vector_test) {
@@ -71,6 +79,12 @@ TEST(print_ip_test_case, print_ip_list_test) {
   std::list<uint32_t> ip2{11111111, 22222222, 33333333, 44444444};
   std::string ip2_str{"0.169.138.199.1.83.21.142.1.252.160.85.2.166.43.28"};
   EXPECT_EQ(get_ip_string(ip2), ip2_str);
+}
+
+TEST(print_ip_test_case, print_ip_tuple_test) {
+  std::tuple<uint8_t, std::string, std::vector<uint16_t>, uint32_t> ip{1, "2.3", {4, 5}, 111111111};
+  std::string ip_str{"1.2.3.0.4.0.5.6.159.107.199"};
+  EXPECT_EQ(get_ip_string(ip), ip_str);
 }
 
 int main(int argc, char *argv[]) {
