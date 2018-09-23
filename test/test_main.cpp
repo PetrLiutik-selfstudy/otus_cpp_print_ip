224ab95fa271cab6290278d9ae682b73dd874b8e
Petr <petr_lutik@mail.ru>
2018-09-23 19:54:45 +0400

    Добавлен тест для вывода char и uint8_t

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -22,6 +22,26 @@ TEST(print_ip_test_case, print_ip_bool_test) {
   bool ip{true};
   print_ip(ss, ip);
   EXPECT_EQ(ss.str(), "1");
+  
+  bool ip{false};
+  print_ip(ss, ip);
+  EXPECT_EQ(ss.str(), "0");
+}
+
+TEST(print_ip_test_case, print_ip_char_test) {
+  std::stringstream ss;
+
+  char ip{-1};
+  print_ip(ss, ip);
+  EXPECT_EQ(ss.str(), "255");
+}
+
+TEST(print_ip_test_case, print_ip_uint8_t_test) {
+  std::stringstream ss;
+  
+  uint8_t ip{23};
+  print_ip(ss, ip);
+  EXPECT_EQ(ss.str(), "23");
 }
 
 int main(int argc, char *argv[]) {
