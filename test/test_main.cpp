11fef75601b5bca3629902d973bcb92f6f383f7c
Petr <petr_lutik@mail.ru>
2018-09-23 21:43:50 +0400

    Добавлен тестовый кейс для списка.

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -40,6 +40,13 @@ TEST(print_ip_test_case, print_ip_int_test) {
   std::string ip2_str{"0"};
   EXPECT_EQ(get_ip_string(ip2), ip2_str);
 
+  char ip3{-1};
+  std::string ip3_str{"255"};
+  EXPECT_EQ(get_ip_string(ip4), ip4_str);
+
+  uint8_t ip4{11};
+  std::string ip4_str{"11"};
+  EXPECT_EQ(get_ip_string(ip4), ip4_str);
 }
 
 TEST(print_ip_test_case, print_ip_vector_test) {
@@ -52,6 +59,15 @@ TEST(print_ip_test_case, print_ip_vector_test) {
   EXPECT_EQ(get_ip_string(ip2), ip2_str);
 }
 
+TEST(print_ip_test_case, print_ip_list_test) {
+  std::list<uint8_t> ip1{1, 2, 3, 4};
+  std::string ip1_str{"1.2.3.4"};
+  EXPECT_EQ(get_ip_string(ip1), ip1_str);
+
+  std::list<uint32_t> ip2{11111111, 22222222, 33333333, 44444444};
+  std::string ip2_str{"0.169.138.199.1.83.21.142.1.252.160.85.2.166.43.28"};
+  EXPECT_EQ(get_ip_string(ip2), ip2_str);
+}
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
