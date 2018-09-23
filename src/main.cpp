5334a4b30a47bad82ef4c16ba5fc53e5cd83e1bd
Petr <petr_lutik@mail.ru>
2018-09-23 17:11:33 +0400

    Причесан вывод ip-адресов

--- src/main.cpp
+++ src/main.cpp
@@ -5,72 +5,41 @@
 
 int main()
 {
-  long long ip1{8875824491850138409};
+  std::cout << "print_ip version: "
+            << ver_major() << "."
+            << ver_minor() << "."
+            << ver_patch() << std::endl;
 
+  char ip1{-1};
   print_ip(std::cout, ip1);
   std::cout << std::endl;
 
-  char ip2{-1};
+  short ip2{0};
   print_ip(std::cout, ip2);
   std::cout << std::endl;
 
-  int8_t ip3{-1};
+  int ip3{2130706433};
   print_ip(std::cout, ip3);
   std::cout << std::endl;
 
-  uint8_t ip4{255};
+  long long ip4{8875824491850138409};
   print_ip(std::cout, ip4);
   std::cout << std::endl;
 
-  auto ip5{65535};
+  std::string ip5{"192.168.1.1"};
   print_ip(std::cout, ip5);
   std::cout << std::endl;
 
-
-  int ip6{2130706433};
+  std::vector<uint8_t> ip6{173, 194, 44, 7};
   print_ip(std::cout, ip6);
   std::cout << std::endl;
 
-  short ip7{0};
+  std::list<int> ip7{2130706433, 2130706433, 2130706433};
   print_ip(std::cout, ip7);
   std::cout << std::endl;
 
-  long long ip8{8875824491850138409};
-
-  print_ip<decltype(ip8), false>(std::cout, ip8);
-  std::cout << std::endl;
-
-  std::string ip9{"192.168.1.1"};
-  print_ip(std::cout, ip9);
-  std::cout << std::endl;
-
-  std::vector<uint8_t> ip10{1, 2, 3, 4};
-  print_ip(std::cout, ip10);
-  std::cout << std::endl;
-
-  std::list<uint8_t> ip11{4, 3, 2, 1};
-  print_ip(std::cout, ip11);
-  std::cout << std::endl;
-
-  std::list<int> ip12{2130706433, 3, 2, 1};
-  print_ip(std::cout, ip12);
-  std::cout << std::endl;
-
-
-  std::vector<std::string> ip13{"192","168","1","1"};
-  print_ip(std::cout, ip13);
-  std::cout << std::endl;
-
-  std::list<std::string> ip14{"192","168","1","1"};
-  print_ip(std::cout, ip14);
-  std::cout << std::endl;
-
-  std::tuple<uint8_t, uint8_t, uint8_t, std::string, std::vector<uint8_t>> ip15{192, 168, 1, "1", {1, 2, 3}};
-  print_ip(std::cout, ip15);
-  std::cout << std::endl;
-
-  bool ip16 = true;
-  print_ip(std::cout, ip16);
+  std::tuple<uint8_t, std::string, std::vector<uint8_t>> ip8{213, "180", {204, 1}};
+  print_ip(std::cout, ip8);
   std::cout << std::endl;
 
   return 0;
